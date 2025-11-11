4267ca8fc9b2741b372ace219eec2f8e8fa9a625
Fernando Araoz <fernando@araozu.dev>
2025-11-10 19:08:04 -0500

    feat: restore expression/function call typechecking

--- src/03_semantic/visitors/typechecker/expression_visitor.zig
+++ src/03_semantic/visitors/typechecker/expression_visitor.zig
@@ -17,35 +17,35 @@ pub fn visit(self: *TypecheckerVisitor, node: *const PrattExpression) VisitorErr
             _ = b;
             std.debug.panic("Not implemented: Typecheck binary expression", .{});
         },
-        .function => std.debug.panic("Not implemented: typechecking function calls", .{}),
-        // .function => |f| {
-        //     // Check that the function_id resolves to a function type
-        //     const t_function_id = try visit_primary_expression(self, &f.primary);
-        //
-        //     // Assert its a function type
-        //     switch (t_function_id) {
-        //         .Function => |t_function| {
-        //             // TODO: assert args are correct when the function calls other things
-        //             return t_function.return_t.*;
-        //         },
-        //         else => {
-        //             // FIXME: compute function_id token range
-        //             var new_error = try self.err.create_and_append_error("Type mismatch in function call", 0, 1);
-        //             // FIXME: show the declaration of the identifier, AND its usage
-        //             {
-        //                 const function_id_tname = t_function_id.to_str();
-        //                 const err_msg = try std.fmt.allocPrint(self.err.allocator, "This expression has type `{s}`, but it is called as a function", .{function_id_tname});
-        //                 const err_msg_label = self.err.create_error_label_alloc(err_msg, 0, 1);
-        //                 try new_error.add_label(err_msg_label);
-        //             }
-        //
-        //             return VisitorError.SemanticError;
-        //         },
-        //     }
-        //
-        //     std.debug.print("TODO: get expression of function call\n", .{});
-        //     return Type.Untyped;
-        // },
+        // .function => std.debug.panic("Not implemented: typechecking function calls", .{}),
+        .function => |*f| {
+            // Check that the function_id resolves to a function type
+            const t_function_id = try visit(self, f.callee);
+
+            // Assert its a function type
+            switch (t_function_id) {
+                .Function => |t_function| {
+                    // TODO: assert args are correct when the function calls other things
+                    return t_function.return_t.*;
+                },
+                else => {
+                    // FIXME: compute function_id token range
+                    var new_error = try self.err.create_and_append_error("Type mismatch in function call", 0, 1);
+                    // FIXME: show the declaration of the identifier, AND its usage
+                    {
+                        const function_id_tname = t_function_id.to_str();
+                        const err_msg = try std.fmt.allocPrint(self.err.allocator, "This expression has type `{s}`, but it is called as a function", .{function_id_tname});
+                        const err_msg_label = self.err.create_error_label_alloc(err_msg, 0, 1);
+                        try new_error.add_label(err_msg_label);
+                    }
+
+                    return VisitorError.SemanticError;
+                },
+            }
+
+            std.debug.print("TODO: get expression of function call\n", .{});
+            return Type.Untyped;
+        },
         .primary => |expr| return try visit_primary_expression(self, expr),
     }
 
