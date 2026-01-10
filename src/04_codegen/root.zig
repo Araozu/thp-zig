f83a4632e7ccd51f9c263123782e037fd526ca67
Fernando Araoz <fernando@araozu.dev>
2026-01-09 20:52:30 -0500

    feat: emit minimal pratt

--- src/04_codegen/root.zig
+++ src/04_codegen/root.zig
@@ -13,6 +13,7 @@ pub const ByteCodeGenerator = struct {
     ast: *const ASTModule,
     allocator: std.mem.Allocator,
     semantic_ctx: *SemanticContext,
+    current_val_reg: u8 = 0,
 
     const Self = @This();
 
@@ -32,16 +33,53 @@ pub const ByteCodeGenerator = struct {
 
         // walk the AST, generate bytecode
         for (self.ast.statements.items) |*statement| {
-            _ = statement;
-            // switch (statement.*) {
-            //     .variableBinding => |b| try self.emit_variable_binding(&chunk, b),
-            //     .expression => |e| try self.emit_pratt_expression(&chunk, e),
-            // }
+            switch (statement.*) {
+                // .variableBinding => |b| try self.emit_variable_binding(&chunk, b),
+                .expression => |e| {
+                    _ = try self.emit_pratt_expression(&chunk, e);
+                },
+                else => @panic("Not implemented in codegen"),
+            }
         }
 
         try chunk.write_opcode(OpCode.op_return);
 
         return chunk;
+    }
+
+    /// What does this do? it computes the bytecode for an expression,
+    /// and has the top of the stack ready to use that computed value
+    fn emit_pratt_expression(self: *Self, chunk: *Chunk, exp: *m_syntax.PrattExpression) !u8 {
+        return switch (exp.*) {
+            // FIXME: ???
+            .primary => |p| try self.emit_primary_expresion(chunk, p.expr),
+            else => @panic("Not implemented: codegen pratt"),
+        };
+    }
+
+    /// Creates the value & returns its register number
+    fn emit_primary_expresion(self: *Self, chunk: *Chunk, exp: *m_syntax.PrimaryExpression) !u8 {
+        switch (exp.*) {
+            // HACK: assumed to be u64
+            .int => |tok_int| {
+                const int_value = try std.fmt.parseInt(u64, tok_int.value, 10);
+
+                // Add to the constants section
+                const constant_idx = try chunk.write_constant(int_value);
+                const reg_number = self.current_val_reg;
+                self.current_val_reg += 1;
+
+                // Save to register
+                try chunk.write_opcode(.op_load);
+                try chunk.write_byte(reg_number);
+                try chunk.write_byte(@intCast(constant_idx));
+
+                return reg_number;
+            },
+            else => {
+                std.debug.panic("Not implemented: codegen other primary expr\n", .{});
+            },
+        }
     }
 
     pub fn deinit() void {
